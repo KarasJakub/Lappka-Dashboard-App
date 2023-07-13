1a8096dc7126590d22f762700efba2b9a5969439
Jakub Karaś <jakub.karas@onet.pl>
2023-07-13 11:25:28 +0200

    feat: removed dummy initial content in storybook

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,13 @@
+import ButtonComponent from "components/global/Button/ButtonComponent.styled";
+
+export default {
+  title: "Button",
+  component: ButtonComponent,
+};
+
+export const Primary = {
+  args: {
+    size: "XLarge",
+    children: "Zaloguj się",
+  },
+};
