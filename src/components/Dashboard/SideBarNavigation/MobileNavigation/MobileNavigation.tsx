d41b9780137b9925033aec82d89d1f2dde12c41a
Jakub Karaś <jakub.karas@onet.pl>
2023-08-01 09:50:34 +0200

    fix: fixed navigation routes

--- src/components/Dashboard/SideBarNavigation/MobileNavigation/MobileNavigation.tsx
+++ src/components/Dashboard/SideBarNavigation/MobileNavigation/MobileNavigation.tsx
@@ -2,36 +2,12 @@ import * as SS from "./MobileNavigation.styled"
 import * as S from "components/Dashboard/SideBarNavigation/SidebarNavigation.styled"
 import { ReactComponent as CompanyLogo } from "assets/icons/CompanyLogo.svg"
 import DashboardNavElement from "components/Dashboard/DashboardNavElement/DashboardNavElement"
-import { ReactComponent as HomeIcon } from "assets/icons/HomeIcon.svg"
-import { ReactComponent as MessageIcon } from "assets/icons/MessageIcon.svg"
-import { ReactComponent as SmillingFaceIcon } from "assets/icons/SimllingFaceIcon.svg"
-import { ReactComponent as HeartIcon } from "assets/icons/HeartIcon.svg"
 import { ReactComponent as UsersIcon } from "assets/icons/UsersIcon.svg"
 import Typography from "components/global/Typography/Typography"
 import theme from "layout/theme"
-
-const NavigationContent = [
-  {
-    icon: <HomeIcon stroke={theme.colors.midGray4} />,
-    name: "Dashboard",
-    path: "/",
-  },
-  {
-    icon: <MessageIcon stroke={theme.colors.midGray4} />,
-    name: "Wiadomości",
-    path: "/messages",
-  },
-  {
-    icon: <SmillingFaceIcon stroke={theme.colors.midGray4} />,
-    name: "Karty zwierząt",
-    path: "/pets",
-  },
-  {
-    icon: <HeartIcon stroke={theme.colors.midGray4} />,
-    name: "Wolontariat",
-    path: "/",
-  },
-]
+import { NavigationContent } from "components/Dashboard/SideBarNavigation/SidebarNavigation"
+import ROUTES from "helpers/utils/routes"
+import UserCard from "components/Dashboard/UserCard/UserCard"
 
 interface Props {
   isMobileNavOpen: boolean
@@ -51,6 +27,7 @@ const MobileNavigation = (props: Props) => {
                 image={item.icon}
                 text={item.name}
                 path={item.path}
+                notifications={item.notifications}
               />
             ))}
             <S.OrganizationPositionWrapper>
@@ -67,10 +44,14 @@ const MobileNavigation = (props: Props) => {
             <DashboardNavElement
               image={<UsersIcon stroke={theme.colors.midGray4} />}
               text="Pracownicy"
-              path="/"
+              path={ROUTES.home}
+              notifications={"0"}
             />
           </S.NavigationList>
         </S.NavigationWrapper>
+        <div style={{ marginTop: "15%" }}>
+          <UserCard />
+        </div>
       </S.NavigationList>
     </SS.MobileNavigationWrapper>
   )
