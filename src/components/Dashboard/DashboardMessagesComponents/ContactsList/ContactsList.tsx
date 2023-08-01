0af31ff299f3ac4384da2c351b44e257338f7aad
Jakub Karaś <jakub.karas@onet.pl>
2023-08-01 09:19:04 +0200

    feat: updated dummy data

--- src/components/Dashboard/DashboardMessagesComponents/ContactsList/ContactsList.tsx
+++ src/components/Dashboard/DashboardMessagesComponents/ContactsList/ContactsList.tsx
@@ -1,14 +1,54 @@
 import * as S from "./ContactsList.styled"
 import ContactItem from "./ContactItem/ContactItem"
 import ArturPhoto from "assets/photos/Messages/ArturPhoto.png"
+import RemigiuszPhoto from "assets/photos/Messages/RemigiuszPhoto.png"
+import KrystynaPhoto from "assets/photos/Messages/KrystynaPhoto.png"
+import SecondArturPhoto from "assets/photos/Messages/SecondArturPhoto.png"
+import SecondRemigiuszPhoto from "assets/photos/Messages/SecondRemigiuszPhoto.png"
+import SecondKRystynaPhoto from "assets/photos/Messages/SecondKrystynaPhoto.png"
 
 const ContactsListDummyData = [
   {
     name: "Artur Pączkowski",
     image: ArturPhoto,
-    message: "hahahahah",
+    message: "Hahahaha 😜😜",
     time: "08:43",
-    messagesCounter: "+2",
+    newMessages: "+2",
+  },
+  {
+    name: "Remigiusz Wątroba",
+    image: RemigiuszPhoto,
+    message: "Świetnie!",
+    time: "08:43",
+    newMessages: "+5",
+  },
+  {
+    name: "Krystyna Nowak",
+    image: KrystynaPhoto,
+    message: "Ta karma jest super...",
+    time: "Pon",
+    newMessages: "+2",
+  },
+  {
+    name: "Artur Pączkowski",
+    image: SecondArturPhoto,
+    message: "Hahahaha 😜😜",
+    time: "08:43",
+    newMessages: "+2",
+  },
+  {
+    name: "Remigiusz Wątroba",
+    image: SecondRemigiuszPhoto,
+    message: "Świetnie!",
+    time: "08:43",
+    newMessages: "+5",
+  },
+  {
+    name: "Krystyna Nowak",
+    image: SecondKRystynaPhoto,
+    message: "Ta karma jest super...",
+    time: "Pon",
+    newMessages: "",
   },
 ]
 
@@ -21,7 +61,7 @@ const ContactsList = () => {
           name={item.name}
           message={item.message}
           time={item.time}
-          messagesCounter={item.messagesCounter}
+          messagesCounter={item.newMessages}
         />
       ))}
     </S.ContactsListWrapper>
