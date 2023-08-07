45920d4699e072858288c7ea375b05d13348ce60
Jakub Karaś <jakub.karas@onet.pl>
2023-08-07 12:31:05 +0200

    feat: added pagination

--- src/components/Dashboard/DashboardPetsCardsComponents/PetsTable/PetsTable.tsx
+++ src/components/Dashboard/DashboardPetsCardsComponents/PetsTable/PetsTable.tsx
@@ -1,46 +1,65 @@
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { PetsContentData, columns, Animal } from "../PetCardsData"
 import {
-  useReactTable,
-  getCoreRowModel,
   flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  PaginationState,
 } from "@tanstack/react-table"
 import CardHeading from "components/global/CardHeading/CardHeading"
 import * as S from "./PetsTable.styled"
+import PetsTableFooter from "./PetsTableFooter/PetsTableFooter"
 
 const PetsTable = () => {
   const data = useMemo(() => PetsContentData, [])
   const columnsMemo = useMemo(() => columns, [])
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  })
+
+  const handlePageSize = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const optionValue = event.target.value
+    setPagination({ ...pagination, pageSize: +optionValue })
+  }
 
   const table = useReactTable({
     data,
     columns: columnsMemo,
     getCoreRowModel: getCoreRowModel<Animal>(),
+    getPaginationRowModel: getPaginationRowModel(),
+    state: {
+      pagination: pagination,
+    },
+    onPaginationChange: setPagination,
   })
   return (
     <S.PetsTableWrapper style={{ width: "100%" }}>
       <CardHeading title="Karty zwierząt" />
       <S.PetsTable style={{ width: "100%" }}>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <S.PetsTableHeadingTr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => {
-              return (
-                <S.PetsTableHeadingTh
-                  key={header.id}
-                  onClick={header.column.getToggleSortingHandler()}
-                >
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                  {{}[header.column.getIsSorted() as string] ?? null}
-                </S.PetsTableHeadingTh>
-              )
-            })}
-          </S.PetsTableHeadingTr>
-        ))}
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <S.PetsTableHeadingTr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <S.PetsTableHeadingTh
+                    key={header.id}
+                    onClick={header.column.getToggleSortingHandler()}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    {{}[header.column.getIsSorted() as string] ?? null}
+                  </S.PetsTableHeadingTh>
+                )
+              })}
+            </S.PetsTableHeadingTr>
+          ))}
+        </thead>
         <tbody>
           {table.getRowModel().rows.map((row) => (
             <S.PetsTableContentTr key={row.id}>
@@ -55,6 +74,12 @@ const PetsTable = () => {
           ))}
         </tbody>
       </S.PetsTable>
+      <PetsTableFooter
+        itemsPerPage={pagination.pageSize}
+        table={table}
+        pagination={pagination}
+        handlePageSize={handlePageSize}
+      />
     </S.PetsTableWrapper>
   )
 }
