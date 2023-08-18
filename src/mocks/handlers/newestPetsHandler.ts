b48f508a9517ad24eec8370d3fbb0624ef06d707
Jakub Karaś <jakub.karas@onet.pl>
2023-08-18 14:59:05 +0200

    fix: minor fix, deleted unused data

--- src/mocks/handlers/newestPetsHandler.ts
+++ src/mocks/handlers/newestPetsHandler.ts
@@ -1,11 +1,7 @@
 import { rest } from "msw"
 import WhiteCatPhoto from "assets/photos/WhiteCatPhoto.png"
 import BlackWhiteCatPhoto from "assets/photos/BlackWhiteCatPhoto.png"
-import DogFrankPhoto from "assets/photos/DogFrankPhoto.png"
-import DogJazzyPhoto from "assets/photos/DogJazzyPhoto.png"
 import DogPhoto from "assets/photos/DogPhoto.png"
-import { PopularPetsCardsContent } from "./mostPopularPetsHandler"
-import FancyPhoto from "assets/photos/Messages/KrystynaPhoto.png"
 
 export interface newestPetsHandlerTypes {
   items: [
@@ -43,29 +39,6 @@ const newestPetsHandlerContent = [
   },
 ]
 
-// const newestPetsHandlerContent = [
-//   {
-//     image: FancyPhoto,
-//     name: "Ninka",
-//     race: "Kot",
-//     date: "06.08.2022",
-//     views: 11200,
-//   },
-//   {
-//     image: FancyPhoto,
-//     name: "Bella",
-//     race: "Pies",
-//     date: "06.08.2022",
-//     views: 1100,
-//   },
-//   {
-//     image: FancyPhoto,
-//     name: "Candy",
-//     race: "Kot",
-//     date: "06.08.2022",
-//     views: 1000,
-//   },
-// ]
 
 export const newestPetsHandler = rest.get<newestPetsHandlerTypes>(
   "/shelters/newestPets",
